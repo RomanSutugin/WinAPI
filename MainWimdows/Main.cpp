a3f8d9a44b84a4b5d23d8304b54a5c0a65429eeb
RomanSutugin <sutuginroman15@gmail.com>
2024-01-09 12:40:52 +0300

    Calc DONE

--- MainWimdows/Main.cpp
+++ MainWimdows/Main.cpp
@@ -1,11 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include<cstdio>
+#include<CommCtrl.h>
 #include "resource.h"
 
+#pragma comment(lib)
+
 CONST CHAR g_sz_WINDOW_CLASS[] = "My first Window";
+TOOLINFO g_toolItem;
+HWND g_hwndTrackingTT;
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HWND CreateTrackingToolTip(INT toolID, HWND hwnd, LPCSTR lpsz_text);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR ipCmdLine, INT nCmdShow)
 {
@@ -72,7 +78,12 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
-		break;
+		g_hwndTrackingTT = CreateTrackingToolTip(IDC_TOOLTIP, hwnd, "MyTooltip");
+		return TRUE;
+	case WM_MOUSELEAVE:
+		SendMessage(g_hwndTrackingTT, TTM_TRACKACTIVATE, FALSE, (LPARAM)&g_toolItem);
+		g_trackingMouse = FALSE;
+	case WM_MOUSEMOVE: 
 	case WM_SIZE:
 	case WM_MOVE:
 	{
@@ -83,7 +94,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		sprintf(sz_title, "%s Position: %ix%i, Size: %ix%i", g_sz_WINDOW_CLASS,
 		rect.left, rect.top,
 		rect.right - rect.left, rect.bottom - rect.top);
-		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)+++sz_title);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
 	}
 	break;
 	case WM_COMMAND:
@@ -93,5 +104,37 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	default:		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return 0;
+}
+
+HWND CreateTrackingToolTip(INT toolID, HWND hwnd, LPSTR lpszText)
+{
+	HWND hwndTT = CreateWindowEx(
+		WS_EX_TOPMOST, TOOLTIPS_CLASS, NULL,
+		WS_POPUP | TTS_NOPREFIX | TTS_ALWAYSTIP,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		hwnd,
+		NULL,
+		GetModuleHandle(NULL),
+		NULL
+
+		);
+
+	if (hwndTT == NULL)
+	{
+		MessageBox(hwnd, "Tooltip creation failed", "Error", MB_OK | MB_ICONERROR);
+			return NULL;
+	}
+
+	g_toolItem.cbSize = sizeof(TOOLINFO);
+	g_toolItem.uFlags = TTF_IDISHWND | TTF_TRACK | TTF_ABSOLUTE;
+	g_toolItem.hwnd = hwnd;
+	g_toolItem.hinst = GetModuleHandle(NULL);
+	g_toolItem.lpszText = lpszText;
+	g_toolItem.uId = (UINT_PTR)hwnd;
+	GetClientRect(hwnd, &g_toolItem.rect);
+
+	//Связать глобальную переменную
+	SendMessage(hwndTT, TTM_ADDTOOL)
 }
 
